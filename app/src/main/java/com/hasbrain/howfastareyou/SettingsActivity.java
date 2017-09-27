f8566f249e62048d3db2c1e59176527afe70f199
ThuyHien261195 <thuyhien261195@gmail.com>
2017-09-27 16:23:41 +0700

    Add SettingsActivity and layout

--- app/src/main/java/com/hasbrain/howfastareyou/SettingsActivity.java
+++ app/src/main/java/com/hasbrain/howfastareyou/SettingsActivity.java
@@ -1,17 +1,91 @@
 package com.hasbrain.howfastareyou;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.hasbrain.howfastareyou.SettingsUtils.DEFAULT_RECORD_STATE;
+import static com.hasbrain.howfastareyou.SettingsUtils.DEFAULT_TIME_LIMIT;
+import static com.hasbrain.howfastareyou.SettingsUtils.MIN_TIME_LIMIT;
+import static com.hasbrain.howfastareyou.SettingsUtils.PREF_RECORD_SCORE;
+import static com.hasbrain.howfastareyou.SettingsUtils.PREF_SETTINGS_FILE;
+import static com.hasbrain.howfastareyou.SettingsUtils.PREF_TIME_LIMIT;
 
 /**
  * Created by Jupiter (vu.cao.duy@gmail.com) on 10/14/15.
  */
 public class SettingsActivity extends AppCompatActivity {
 
+    @BindView(R.id.seek_bar_time_limit)
+    SeekBar seekBarTimeLimit;
+
+    @BindView(R.id.text_time_limit)
+    TextView textViewTimeLimit;
+
+    @BindView(R.id.switch_record_score)
+    SwitchCompat switchRecordScore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_count);
+        setContentView(R.layout.activity_settings);
         setTitle(R.string.settings_text);
+
+        ButterKnife.bind(this);
+        initViews();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        storeSettingsVariable();
+    }
+
+    private void storeSettingsVariable() {
+        SettingsUtils.recordState = switchRecordScore.isChecked();
+        TapCountActivity.TIME_COUNT = SettingsUtils.timeLimit * SettingsUtils.numToConvertSec;
+
+        SharedPreferences sharedPref = this.getSharedPreferences(PREF_SETTINGS_FILE, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(SettingsUtils.PREF_TIME_LIMIT, SettingsUtils.timeLimit);
+        editor.putBoolean(SettingsUtils.PREF_RECORD_SCORE, SettingsUtils.recordState);
+        editor.apply();
+    }
+
+    private void initViews() {
+        switchRecordScore.setChecked(SettingsUtils.recordState);
+        textViewTimeLimit.setText(getString(R.string.title_result_time_limit, SettingsUtils.timeLimit));
+        if (SettingsUtils.timeLimit <= MIN_TIME_LIMIT) {
+            seekBarTimeLimit.setProgress(0);
+        } else {
+            seekBarTimeLimit.setProgress(SettingsUtils.timeLimit);
+        }
+        seekBarTimeLimit.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress < SettingsUtils.MIN_TIME_LIMIT) {
+                    progress = SettingsUtils.MIN_TIME_LIMIT;
+                }
+                SettingsUtils.timeLimit = progress;
+                textViewTimeLimit.setText(getString(R.string.title_result_time_limit, progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/hasbrain/howfastareyou/SettingsUtils.java
@@ -0,0 +1,17 @@
+package com.hasbrain.howfastareyou;
+
+/**
+ * Created by thuyhien on 9/27/17.
+ */
+
+public class SettingsUtils {
+    public static final String PREF_SETTINGS_FILE = "SettingsPrefFile";
+    public static final String PREF_TIME_LIMIT = "TimeLimit";
+    public static final String PREF_RECORD_SCORE = "RecordScore";
+    public static final int MIN_TIME_LIMIT = 5;
+    public static final int DEFAULT_TIME_LIMIT = 10;
+    public static final boolean DEFAULT_RECORD_STATE = true;
+    public static final int numToConvertSec = 1000;
+    public static int timeLimit = 10;
+    public static boolean recordState = true;
+}
