ec65b9304449161607e8036b0c5307a1bb91c510
ThuyHien261195 <thuyhien261195@gmail.com>
2017-09-28 13:47:01 +0700

    Add realProgress variable to check value seekbar whether changed or not, return result if has change value

--- app/src/main/java/com/hasbrain/howfastareyou/SettingsActivity.java
+++ app/src/main/java/com/hasbrain/howfastareyou/SettingsActivity.java
@@ -1,6 +1,5 @@
 package com.hasbrain.howfastareyou;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,12 +10,8 @@ import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-import static com.hasbrain.howfastareyou.SettingsUtils.DEFAULT_RECORD_STATE;
-import static com.hasbrain.howfastareyou.SettingsUtils.DEFAULT_TIME_LIMIT;
 import static com.hasbrain.howfastareyou.SettingsUtils.MIN_TIME_LIMIT;
-import static com.hasbrain.howfastareyou.SettingsUtils.PREF_RECORD_SCORE;
 import static com.hasbrain.howfastareyou.SettingsUtils.PREF_SETTINGS_FILE;
-import static com.hasbrain.howfastareyou.SettingsUtils.PREF_TIME_LIMIT;
 
 /**
  * Created by Jupiter (vu.cao.duy@gmail.com) on 10/14/15.
@@ -32,6 +27,9 @@ public class SettingsActivity extends AppCompatActivity {
     @BindView(R.id.switch_record_score)
     SwitchCompat switchRecordScore;
 
+    private SettingsModel settingsModel;
+    private int realProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,41 +37,57 @@ public class SettingsActivity extends AppCompatActivity {
         setTitle(R.string.settings_text);
 
         ButterKnife.bind(this);
+        settingsModel = SettingsUtils.getSettingsModel(this);
         initViews();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        storeSettingsVariable();
+    }
+
+    @Override
+    public void onBackPressed() {
+        checkSettingsChange();
+        super.onBackPressed();
+    }
+
+    private void checkSettingsChange() {
+        if (realProgress != settingsModel.getTimeLimit() ||
+                switchRecordScore.isChecked() != settingsModel.isRecordState()) {
+            storeSettingsVariable();
+            setResult(RESULT_OK);
+        } else {
+            setResult(RESULT_CANCELED);
+        }
     }
 
     private void storeSettingsVariable() {
-        SettingsUtils.recordState = switchRecordScore.isChecked();
-        TapCountActivity.TIME_COUNT = SettingsUtils.timeLimit * SettingsUtils.numToConvertSec;
-
         SharedPreferences sharedPref = this.getSharedPreferences(PREF_SETTINGS_FILE, MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt(SettingsUtils.PREF_TIME_LIMIT, SettingsUtils.timeLimit);
-        editor.putBoolean(SettingsUtils.PREF_RECORD_SCORE, SettingsUtils.recordState);
+        editor.putInt(SettingsUtils.PREF_TIME_LIMIT, realProgress);
+        editor.putBoolean(SettingsUtils.PREF_RECORD_SCORE, switchRecordScore.isChecked());
         editor.apply();
     }
 
     private void initViews() {
-        switchRecordScore.setChecked(SettingsUtils.recordState);
-        textViewTimeLimit.setText(getString(R.string.title_result_time_limit, SettingsUtils.timeLimit));
-        if (SettingsUtils.timeLimit <= MIN_TIME_LIMIT) {
+        switchRecordScore.setChecked(settingsModel.isRecordState());
+        realProgress = settingsModel.getTimeLimit();
+        textViewTimeLimit.setText(getString(R.string.title_result_time_limit, realProgress));
+
+        if (settingsModel.getTimeLimit() <= MIN_TIME_LIMIT) {
             seekBarTimeLimit.setProgress(0);
         } else {
-            seekBarTimeLimit.setProgress(SettingsUtils.timeLimit);
+            seekBarTimeLimit.setProgress(settingsModel.getTimeLimit());
         }
+
         seekBarTimeLimit.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 if (progress < SettingsUtils.MIN_TIME_LIMIT) {
                     progress = SettingsUtils.MIN_TIME_LIMIT;
                 }
-                SettingsUtils.timeLimit = progress;
+                realProgress = progress;
                 textViewTimeLimit.setText(getString(R.string.title_result_time_limit, progress));
             }
 
