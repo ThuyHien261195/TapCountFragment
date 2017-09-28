380216917f353d3c2f99e52e14a347b6d65671c9
ThuyHien261195 <thuyhien261195@gmail.com>
2017-09-28 13:39:25 +0700

    Create public function getSettingsModel

--- app/src/main/java/com/hasbrain/howfastareyou/SettingsUtils.java
+++ app/src/main/java/com/hasbrain/howfastareyou/SettingsUtils.java
@@ -1,5 +1,10 @@
 package com.hasbrain.howfastareyou;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import static android.content.Context.MODE_PRIVATE;
+
 /**
  * Created by thuyhien on 9/27/17.
  */
@@ -11,7 +16,11 @@ public class SettingsUtils {
     public static final int MIN_TIME_LIMIT = 5;
     public static final int DEFAULT_TIME_LIMIT = 10;
     public static final boolean DEFAULT_RECORD_STATE = true;
-    public static final int numToConvertSec = 1000;
-    public static int timeLimit = 10;
-    public static boolean recordState = true;
+
+    public static SettingsModel getSettingsModel(Context context) {
+        SharedPreferences sharedPref = context.getSharedPreferences(PREF_SETTINGS_FILE, MODE_PRIVATE);
+        int timeLimit = sharedPref.getInt(PREF_TIME_LIMIT, DEFAULT_TIME_LIMIT);
+        boolean recordState = sharedPref.getBoolean(PREF_RECORD_SCORE, DEFAULT_RECORD_STATE);
+        return new SettingsModel(timeLimit, recordState);
+    }
 }
