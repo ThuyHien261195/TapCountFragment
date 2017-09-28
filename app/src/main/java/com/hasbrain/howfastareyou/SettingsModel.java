b504e497561bd4f62c929cc97202153bc5fb32ac
ThuyHien261195 <thuyhien261195@gmail.com>
2017-09-28 13:37:07 +0700

    Add SettingsModel file

---
+++ app/src/main/java/com/hasbrain/howfastareyou/SettingsModel.java
@@ -0,0 +1,31 @@
+package com.hasbrain.howfastareyou;
+
+/**
+ * Created by thuyhien on 9/28/17.
+ */
+
+public class SettingsModel {
+    private int timeLimit;
+    private boolean recordState;
+
+    public SettingsModel(int timeLimit, boolean recordState) {
+        this.timeLimit = timeLimit;
+        this.recordState = recordState;
+    }
+
+    public int getTimeLimit() {
+        return timeLimit;
+    }
+
+    public void setTimeLimit(int timeLimit) {
+        this.timeLimit = timeLimit;
+    }
+
+    public boolean isRecordState() {
+        return recordState;
+    }
+
+    public void setRecordState(boolean recordState) {
+        this.recordState = recordState;
+    }
+}
