6a90b69775163d343d5efd3065628575b50632fd
ThuyHien261195 <thuyhien261195@gmail.com>
2017-09-29 17:33:11 +0700

    Add close FileOutputStream and FileInputStream

--- app/src/main/java/com/hasbrain/howfastareyou/Utils/FileProvider.java
+++ app/src/main/java/com/hasbrain/howfastareyou/Utils/FileProvider.java
@@ -9,12 +9,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 /**
@@ -47,6 +49,7 @@ public class FileProvider {
                         highScoreList.add(highScore);
                     }
                 }
+                fileInputStream.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -60,7 +63,12 @@ public class FileProvider {
             try {
                 FileOutputStream fileOutputStream = createFileOutputStream(context);
                 if (fileOutputStream != null) {
-                    fileOutputStream.write((jsonHighScore.toString() + "\n").getBytes());
+                    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+                    BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+                    bufferedWriter.write(jsonHighScore.toString());
+                    bufferedWriter.newLine();
+                    bufferedWriter.close();
+                    fileOutputStream.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
